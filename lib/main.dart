f42bd1f4806fe779463ea64ddd617c707c539d61
Atmakendric <bapanmandal86958@gmail.com>
2023-02-11 20:41:50 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,253 @@
+import 'package:bmi/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: splash_screen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+   var heightTextft=TextEditingController();
+   var heightTextin=TextEditingController();
+   var weightText=TextEditingController();
+   var result="";
+   var bgColor=Colors.indigo.shade200;
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+
+        title: Text("BMI Calculator"),
+        titleSpacing: 21,
+        titleTextStyle: TextStyle(fontSize: 27,fontWeight: FontWeight.bold),
+      ),
+      body: Container(
+        color: bgColor,
+        child: Center(
+          child: Container(
+            width: 350,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'BMI',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 34),
+                ),
+                SizedBox(height: 18,),
+                TextField(
+                  controller: weightText,
+                  keyboardType: TextInputType.number,
+                  // style: TextStyle(fontSize: 17,fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    label: Text('Enter Your Weight in Kgs'),
+                    prefixIcon:  Icon(Icons.line_weight),
+                    // enabledBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.green,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.blue,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedErrorBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.red,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // border: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.blue,
+                    //       width: 2
+                    //   ),
+                    // ),
+                  ),
+                ),
+                SizedBox(height: 13,),
+                TextField(
+                  controller: heightTextft,
+                  keyboardType: TextInputType.number,
+                  // style: TextStyle(fontSize: 17,fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    label: Text('Enter Your Height in ft'),
+                    prefixIcon:  Icon(Icons.height),
+                    // enabledBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.green,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.blue,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedErrorBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.red,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // border: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //     color: Colors.blue,
+                    //     width: 2
+                    //   ),
+                    // ),
+                  ),
+                ),
+                SizedBox(height: 13,),
+                TextField(
+                  controller: heightTextin,
+                  keyboardType: TextInputType.number,
+                  // style: TextStyle(fontSize: 17,fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    label: Text('Enter Your Height in inch'),
+                    prefixIcon:  Icon(Icons.height),
+                    // enabledBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.green,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.blue,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // focusedErrorBorder: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.red,
+                    //       width: 2
+                    //   ),
+                    // ),
+                    // border: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(10),
+                    //   borderSide: BorderSide(
+                    //       color: Colors.blue,
+                    //       width: 2
+                    //   ),
+                    // ),
+                  ),
+                ),
+                SizedBox(height: 15,),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding:EdgeInsets.all(15),
+                      // backgroundColor: Color.fromARGB(400, 6, 7, 9),
+                    ),
+                    onPressed: (){
+                       var ft= heightTextft.text.toString();
+                       var inch= heightTextin.text.toString();
+                       var weight= weightText.text.toString();
+
+                       if(ft!=""&&inch!=""&&weight!=""){
+                       //
+                         var ftInt=int.parse(ft);
+                         var inchInt=int.parse(inch);
+                         var weightInt=int.parse(weight);
+                         var tInch=(ftInt*12)+inchInt;
+                         var tCm=tInch*2.54;
+                         var tM=tCm/100;
+                         var bmi=weightInt/(tM*tM);
+                         var msg="";
+                         if(bmi>25){
+                           msg="You are OverWeight.";
+                           bgColor=Colors.orange.shade200;
+                         }else if(bmi<18){
+                           msg='You are UnderWeight.';
+                           bgColor=Colors.red.shade200;
+                         }else{
+                           msg ="You are Healthy.";
+                           bgColor=Colors.green.shade200;
+                         }
+                         setState(() {
+                           result="$msg\nYour BMI is ${bmi.toStringAsFixed(3)} kg/m2";
+                         });
+                       }else{
+                         setState(() {
+                           result="Please, enter all required details!";
+                         });
+                       }
+                }
+                    , child:Text('Calculate',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22),) ),
+                  SizedBox(height: 10,),
+                   Text(result,style: TextStyle(fontSize: 25),),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+
+
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bmi/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class splash_screen extends StatefulWidget{
+  @override
+  State<splash_screen> createState() => _splash_screenState();
+}
+
+class _splash_screenState extends State<splash_screen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(milliseconds: 2000), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: "BMI Calculator"),));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Center(child: Text('BMI',style: TextStyle(
+            color: Colors.white60,
+            fontWeight: FontWeight.w700,
+            fontSize: 100,
+            fontFamily: 'Raleway',
+        ),)),
+      ),
+    );
+  }
+}
